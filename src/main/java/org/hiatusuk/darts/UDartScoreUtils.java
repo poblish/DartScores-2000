885baea51e095154514c98ebeae9ad8273efd759
Andrew Regan <aregan@gmail.com>
2018-01-14 22:57:48 +0000

    Tabs to spaces

--- src/main/java/org/hiatusuk/darts/UDartScoreUtils.java
+++ src/main/java/org/hiatusuk/darts/UDartScoreUtils.java
@@ -8,40 +8,40 @@ import java.util.Vector;
  *******************************************************************************/
 public class UDartScoreUtils
 {
-	/*******************************************************************************
-	 *******************************************************************************/
-	public static void RemoveAllDuplicates( Vector inVector)
-	{
-		DartScoreSet	iScore, jScore;
-		int				lastIndex = inVector.size() - 1;
-		boolean			endIt = false;
+    /*******************************************************************************
+     *******************************************************************************/
+    public static void RemoveAllDuplicates( Vector inVector)
+    {
+        DartScoreSet    iScore, jScore;
+        int             lastIndex = inVector.size() - 1;
+        boolean         endIt = false;
 
-		for ( int i = 0; i < lastIndex; i++)
-		{
-			try
-			{
-				iScore = (DartScoreSet) inVector.elementAt(i);
+        for ( int i = 0; i < lastIndex; i++)
+        {
+            try
+            {
+                iScore = (DartScoreSet) inVector.elementAt(i);
 
-				for ( int j = i + 1; j <= lastIndex; j++)
-				{
-					try
-					{
-						//		System.out.println( i + "," + j);
-						jScore = (DartScoreSet) inVector.elementAt(j);
-						if ( jScore.equals(iScore))
-						{
-							inVector.removeElement(jScore);
-							--j;
-						}
-					}
-					catch ( ArrayIndexOutOfBoundsException e)
-					{
-					}
-				}
-			}
-			catch ( ArrayIndexOutOfBoundsException e)
-			{
-			}
-		}
-	}
+                for ( int j = i + 1; j <= lastIndex; j++)
+                {
+                    try
+                    {
+                        // System.out.println( i + "," + j);
+                        jScore = (DartScoreSet) inVector.elementAt(j);
+                        if ( jScore.equals(iScore))
+                        {
+                            inVector.removeElement(jScore);
+                            --j;
+                        }
+                    }
+                    catch ( ArrayIndexOutOfBoundsException e)
+                    {
+                    }
+                }
+            }
+            catch ( ArrayIndexOutOfBoundsException e)
+            {
+            }
+        }
+    }
 }
