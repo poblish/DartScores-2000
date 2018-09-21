f1da38f8475e9be2fa0fa475e21d20c0021f897e
Andrew Regan <aregan@gmail.com>
2018-09-21 23:21:35 +0100

    Update deps; Spring Boot 2.x

--- src/main/java/sample/jsp/Darts2000Application.java
+++ src/main/java/sample/jsp/Darts2000Application.java
@@ -3,7 +3,7 @@ package sample.jsp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Darts2000Application extends SpringBootServletInitializer {
